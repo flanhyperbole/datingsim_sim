49e6b1f290a4703c353423c11d251f165bca8967
karasu <karasu@bigbox>
2023-05-06 17:56:57 +1200

    initial attraction setups and tests

--- datingsim_sim/main.py
+++ datingsim_sim/main.py
@@ -11,6 +11,8 @@ import random
     # individual meets
     # group scenes
 
+    # an event will one, two or three stats, which will define how character interact with it
+
     # Depending on the attraction score between each charater could do:
     # attract
     # sabbotage
@@ -49,22 +51,21 @@ class Stats:
 
     def __repr__(self) -> str:
         return f"""
-            extraversion <-> intraversion {self.extraversion_intraversion}
-            openess <-> calculation = {self.openess_calculation}
-            emotion <-> intelligence = {self.emotion_intelligence}
+            extraversion {Stats.statbar(self.extraversion_intraversion)} intraversion
+            openess      {Stats.statbar(self.openess_calculation)} calculation
+            emotion      {Stats.statbar(self.emotion_intelligence)} intelligence
         """
     
     @staticmethod
     def statbar(stat):
         reverse = stat < 0
         stat_abs = abs(stat)
-        statrange = [i for i in range(0, stat_abs)]
-        print(statrange)
+        statrange = [i for i in range(0, stat_abs+1)]
         barlist = ['.' for i in Stats.STATRANGE]
         for i in statrange:
-            barlist[i] = '|'
+            barlist[i+5] = '|'
         if reverse:
-            barlist = barlist[::1]
+            barlist = barlist[::-1]
         return f"[{''.join(barlist)}]"
 
 
@@ -77,42 +78,85 @@ class Stats:
         else:
             stat = random.choice(Stats.STATRANGE)
         return int(stat)
+    
+    def get_stat_iter(self):
+        return [
+            self.extraversion_intraversion
+            , self.openess_calculation
+            , self.emotion_intelligence
+        ]
+    
 
 class Character:
-    def __init__(self, name):
+    def __init__(self, name, prompt=True):
         self.name = name
+        self.prompt = prompt
         self.attraction = {}
         self.stats = self._set_stats()
 
     def __repr__(self) -> str:
         return f"""
             Character: {self.name}
-            {self.stats=}
+            stats: {self.stats}
         """
 
-    def _set_stats(self):
-        randomise = input('would you like to set this characters stats? yes/no').lower().strip() != 'yes' 
-        return Stats(randomise)
+    @classmethod
+    def create_character_with_stats(
+        cls
+        , name
+        , extraversion_intraversion = 0
+        , openess_calculation = 0
+        , emotion_intelligence = 0
+    ):
+        anon = Character(name, False)
+        anon.stats.extraversion_intraversion = extraversion_intraversion
+        anon.stats.openess_calculation       = openess_calculation 
+        anon.stats.emotion_intelligence      = emotion_intelligence
+        return anon
 
+    def _set_stats(self):
+        randomise = True if not self.prompt else input('would you like to set this characters stats? yes/no').lower().strip() != 'yes' 
+        return Stats(randomise)
+    
+    def _add_attraction(self, other):
+        self.attraction[other.name] = 0 
+        self.calculate_attraction(other)
+    
+    def calculate_attraction(self, other):
+        #total attraction is the inverse of the distance between two characters stats
+        attraction = 0
+        for stat_compare in zip(self.stats.get_stat_iter(), other.stats.get_stat_iter()):
+            stat_diff = stat_compare[0] - stat_compare[1]
+            attraction += (10 - stat_diff)
+        self.attraction[other.name] = attraction
 #%%
 
 class Game:
-    def __init__(self):
-        self.characters = []
+    def __init__(self, prompt=True):
+        self.characters = {}
         self.round = 0
-        while True:
-            if len(self.characters) < 3:
-                self.add_character()
-            else:
-                add = input('add another charater (yes/no)').lower().strip() == 'yes'
-                if add:
+        if prompt:
+            while True:
+                if len(self.characters) < 3:
                     self.add_character()
                 else:
-                    break
+                    add = input('add another charater (yes/no)').lower().strip() == 'yes'
+                    if add:
+                        self.add_character()
+                    else:
+                        break
+
+    def _set_initial_attraction(self):        
+        for character_name, character in self.characters.items():
+            for other_character_name, other_character in self.characters.items():
+                if character_name == other_character_name:
+                    continue
+                else:
+                    character._add_attraction(other_character)
 
     def add_character(self):
         name = input(f'charater {len(self.characters) + 1} name:')
-        self.characters.append(Character(name)) 
+        self.characters[name] = Character(name) 
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,33 @@
+import pytest
+
 from datingsim_sim.main import *
+
+@pytest.fixture
+def normal_amy():
+    return Character.create_character_with_stats('normal_amy')  
+
+@pytest.fixture
+def normal_bob():
+    return Character.create_character_with_stats('normal_bob')    
+
+@pytest.fixture
+def introvert_cas():  
+    return Character.create_character_with_stats('introvert_cas', 5, 5, 5)
+
+@pytest.fixture
+def extrovert_dan():
+    return Character.create_character_with_stats('extrovert_dan', -5, -5, -5)
+
+@pytest.fixture
+def four_character_game(normal_amy, normal_bob, introvert_cas, extrovert_dan):
+    game = Game(False)
+    game.characters[normal_amy.name] = normal_amy
+    game.characters[normal_bob.name] = normal_bob
+    game.characters[introvert_cas.name] = introvert_cas
+    game.characters[extrovert_dan.name] = extrovert_dan
+    return game
+
+## the tests
 
 def test_charater_random(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: 'no')
@@ -14,3 +43,20 @@ def test_chatacter_set_stats(monkeypatch):
     assert character.stats.extraversion_intraversion == -5
     assert character.stats.openess_calculation == 0
     assert character.stats.emotion_intelligence == 3
+
+def test_statbar_display():
+    statbar = Stats.statbar(3)
+    assert statbar == '[.....||||..]'
+
+def test_normals_have_equal_stats(normal_bob, normal_amy):
+    for stat_compare in zip(normal_bob.stats.get_stat_iter(), normal_amy.stats.get_stat_iter()):
+        assert stat_compare[0] == stat_compare[1]
+
+def test_setup_attraction(four_character_game):
+    four_character_game._set_initial_attraction()
+    assert 'normal_amy' in four_character_game.characters['normal_bob'].attraction
+    assert four_character_game.characters['normal_bob'].attraction['normal_amy'] == 30
+    assert four_character_game.characters['introvert_cas'].attraction['extrovert_dan'] == 0
+    assert four_character_game.characters['introvert_cas'].attraction['normal_amy'] == 15
+     
+
